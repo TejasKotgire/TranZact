8affec86a15d3597c171a191acd03ca58f1c4689
Tejas Kotgire <tejaskotgire@gmail.com>
2024-12-12 21:10:23 +0530

    CORS error fixed

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express()
 
 dotenv.config();
 connectDB()
-app.use(cors());
+app.use(cors({
+    origin: 'https://tran-zact.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true                            
+  }));
 app.use(express.json())
 
 app.use('/api/v1', rootRouter)

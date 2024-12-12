21e5842ab024ef427617b4ecd95ee1f9f867c63b
Tejas Kotgire <tejaskotgire@gmail.com>
2024-12-12 20:19:54 +0530

    Fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,11 @@ const app = express()
 
 dotenv.config();
 connectDB()
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    credentials: true                            
+  }));
 app.use(express.json())
 
 app.use('/api/v1', rootRouter)

27e6a2588975f7335ea4271e6c1f8f91b0069fc3
Tejas Kotgire <tejaskotgire@gmail.com>
2024-12-12 19:26:30 +0530

    fixing cors

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,16 @@ const connectDB = require("./db/connectDB.js");
 
 const app = express()
 
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+};
+  
+app.use(cors(corsOptions));
+
 dotenv.config();
 connectDB()
-app.use(cors())
 app.use(express.json())
 
 app.use('/api/v1', rootRouter)
